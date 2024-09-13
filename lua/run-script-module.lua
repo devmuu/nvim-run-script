5b8d63216fa45f7854207c56fb596bcdd3b602dc
devmuu <m.andrade@alu.ufc.br>
2024-09-13 15:58:02 -0300

    new test 004

--- lua/run-script-module.lua
+++ lua/run-script-module.lua
@@ -6,5 +6,6 @@ function M.info()
     print("This is run-script module")
 end
 
+vim.api.nvim_create_user_command("InfoTestRun", info, {})
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,4 @@
 local runscript = require("run-script-module")
 
 vim.cmd [[command! RunScriptInfo lua require("run-script-module").info()]]
+vim.cmd [[command! RunScriptInfoTest lua runscript.info()]]
