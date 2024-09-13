53a2773cc9d890953e560845556d1089eaa172f8
devmuu <m.andrade@alu.ufc.br>
2024-09-13 15:58:53 -0300

    new test 004

--- lua/run-script-module.lua
+++ lua/run-script-module.lua
@@ -6,6 +6,6 @@ function M.info()
     print("This is run-script module")
 end
 
-vim.api.nvim_create_user_command("InfoTestRun", info, {})
+vim.api.nvim_create_user_command("InfoTestRun", M.info, {})
 
 return M
