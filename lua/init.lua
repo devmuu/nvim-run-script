3e84baab8b68fe1da9001d16c2ca11a9ff9e18d4
devmuu <m.andrade@alu.ufc.br>
2024-09-13 15:09:52 -0300

    change user command to vim command

--- lua/init.lua
+++ lua/init.lua
@@ -1,5 +1,3 @@
 require("run_script_module")
 
-vim.api.nvim_create_user_command("RunScriptInfo", function()
-    require("run_script_module").info()
-end, {})
+vim.cmd [[ command! RunScriptInfo lua require("run_script_module").info() ]]
