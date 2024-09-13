60b8f082eef9a98e9aa71563c03d1bae28f887c7
devmuu <m.andrade@alu.ufc.br>
2024-09-13 17:51:17 -0300

    update functions

--- plugin/init.lua
+++ plugin/init.lua
@@ -7,6 +7,10 @@ vim.api.nvim_create_user_command("RunLineScript", runscript.runLine, {})
 vim.api.nvim_create_user_command("RunFileScript", runscript.runFile, {})
 vim.api.nvim_create_user_command("ClearFileScript", runscript.clearFile, {})
 
+vim.api.nvim_create_user_command('ClearFileScript', function ()
+    runscript.clearFile
+end, { desc = "Clear temp file", nargs = '' })
+
 -- run current line
 map("n", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
 
