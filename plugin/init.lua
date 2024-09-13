fa75d23e2bacb6632ea11e7178d7848ed1036796
devmuu <m.andrade@alu.ufc.br>
2024-09-13 16:10:47 -0300

    create nvim command

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,4 +1,3 @@
 local runscript = require("run-script-module")
 
-vim.api.nvim_create_user_command("InfoTestRun", runscript.info, {})
-vim.cmd [[command! RunScriptInfo lua require("run-script-module").info()]]
+vim.api.nvim_create_user_command("InfoRunScript", runscript.info, {})
