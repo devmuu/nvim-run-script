275b98e112d5a19fd2a97061d36bb0022b8dd87b
devmuu <m.andrade@alu.ufc.br>
2024-09-13 16:53:12 -0300

    update functions

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,16 +2,16 @@ local runscript = require("run-script-module")
 local map = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = true}
 
-vim.api.nvim_create_user_command("InfoRunScript", runscript.info, {})
-vim.api.nvim_create_user_command("RunLineScript", runscript.runLine, {})
-vim.api.nvim_create_user_command("RunFileScript", runscript.runFile, {})
-vim.api.nvim_create_user_command("ClearFileScript", runscript.clearFile, {})
+vim.api.nvim_create_user_command("InfoRunScript", runscript.info(), {})
+vim.api.nvim_create_user_command("RunLineScript", runscript.runLine(), {})
+vim.api.nvim_create_user_command("RunFileScript", runscript.runFile(), {})
+vim.api.nvim_create_user_command("ClearFileScript", runscript.clearFile(), {})
 
 -- run current line
 map("n", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
 
 -- run file from selected lines
-map("x", "<c-CR>", "<Cmd>lua runscript.runFile<CR>", opts)
+map("x", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
 
 -- clean script temp file
 map("n", "<F9>", "<Cmd>ClearFileScript<CR>", opts)
