a1a11c2b853461f82d58ca24da03c4542877a582
devmuu <m.andrade@alu.ufc.br>
2025-03-13 20:30:05 -0300

    remove require in clean call function

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,6 +1,6 @@
 local runscript = require("run-script-module")
 local map = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 vim.api.nvim_create_user_command("InfoRunScript", runscript.info, {})
 vim.api.nvim_create_user_command("RunLineScript", runscript.runLine, {})
@@ -10,7 +10,7 @@ vim.api.nvim_create_user_command("ClearFileScript", runscript.clearFile, {})
 vim.api.nvim_create_user_command('ClearFileScript', function()
     require('run-script-module').clearFile()
     print("Temporary file cleared!")
-end, { desc = "Clear temp file"})
+end, { desc = "Clear temp file" })
 
 -- run current line
 map("n", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
@@ -19,4 +19,4 @@ map("n", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
 map("x", "<c-CR>", "<Cmd>RunFileScript<CR>", opts)
 
 -- clean script temp file
-map("n", "<F9>", "<Cmd>lua require('run-script-module').clearFile()<CR>", opts)
+map("n", "<F9>", "<Cmd>ClearFileScript<CR>", opts)
